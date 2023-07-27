34169dbafc1aa74e20f49a16b2b1943e04cf8c5c
tulio <tuliomoya17@gmail.com>
2023-07-27 11:53:44 +0930

    integrador

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class MinMaxFinder
+{
+    public static void FindMinMax(int[] numbers, out int min, out int max)
+    {
+        if (numbers == null || numbers.Length == 0)
+            throw new ArgumentException("El array no puede estar vacío o ser nulo.");
+
+        min = numbers[0];
+        max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number < min)
+                min = number;
+
+            if (number > max)
+                max = number;
+        }
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        int[] numbers = { 10, 5, 20, 15, 30, 25 };
+        int min, max;
+        MinMaxFinder.FindMinMax(numbers, out min, out max);
+        Console.WriteLine($"El valor mínimo es: {min}");
+        Console.WriteLine($"El valor máximo es: {max}");
+        Console.WriteLine("Presiona Enter para salir...");
+        Console.ReadLine();
+    }
+}
+
---
+++ TPIntegral.P2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPIntegral.P2
+{
+
+    public class FactorialCalculator
+    {
+        public static int CalculateFactorial(int number)
+        {
+            if (number < 0)
+                throw new ArgumentException("El número debe ser mayor o igual a cero.");
+
+            if (number == 0 || number == 1)
+                return 1;
+
+            int factorial = 1;
+            for (int i = 2; i <= number; i++)
+            {
+                factorial *= i;
+            }
+
+            return factorial;
+        }
+    }
+
+    public class Program
+    {
+        public static void Main()
+        {
+            int number = 8;
+            int factorial = FactorialCalculator.CalculateFactorial(number);
+            Console.WriteLine($"El factorial de {number} es: {factorial}");
+            Console.WriteLine("Presiona Enter para salir...");
+            Console.ReadLine();
+        }
+    }
+
+
+
+}
